29a68cd5e9cc0c8887e81575d42da1d3e11d14a4
jade21345 <Jade.Coulson@Yahoo.co.uk>
2026-05-03 16:00:19 +0100

    added language toggle button

--- app/script.js
+++ app/script.js
@@ -1,3 +1,80 @@
-function showMessage() {
-    alert("Our redesigned system improves fairness and accessibility!");
+
+let currentLang = "en";
+
+if (document.getElementById('map')) {
+var map = L.map('map',{
+    dragging: false,
+    zoomControl: false,
+    scrollWheelZoom: false,
+    doubleClickZoom: false,
+    //  boxZoom: false,
+    //  keyboard: false,
+    //  tap: false,
+    touchZoom: false}
+).setView([39.5, -8], 7);
+
+
+
+var stats = {
+    "Lisboa": "Turnout: 68%",
+    "Porto": "Turnout: 64%"
+};
+
+fetch("portugal_district.geojson")
+    .then(res => res.json())
+    .then(data => {
+
+        function style(feature) {
+            return {
+                color: "#333",
+                weight: 1,
+                fillColor: "#66cc66",
+                fillOpacity: 0.6
+            };
+        }
+
+        function onEachFeature(feature, layer) {
+
+            layer.on({
+                mouseover: function (e) {
+                    var name = feature.properties.name;
+                    var info = stats[name] ;
+
+                    layer.bindTooltip(name + "<br>" + info).openTooltip();
+                    layer.setStyle({ fillColor: "#ffffff" });
+                },
+
+                mouseout: function (e) {
+                    geojson.resetStyle(layer);
+                }
+            });
+        }
+
+        var geojson = L.geoJSON(data, {
+            style: style,
+            onEachFeature: onEachFeature
+        }).addTo(map);
+
+    });
 }
+
+function toggleLanguage() {
+    currentLang = currentLang === "en" ? "pt" : "en";
+
+    const elements = document.querySelectorAll("[data-en]");
+    elements.forEach(el => {
+        el.textContent = el.getAttribute(`data-${currentLang}`);
+    });
+
+    updateButton();
+}
+
+function updateButton() {
+    const btn = document.querySelector(".nav-right button");
+    if (!btn) return;
+    btn.innerHTML = currentLang === "en" 
+        ? '<img src="images/icon_portugal.png" alt="PT">' 
+        : '<img src="images/icon_uk.png" alt="EN">';
+}
+
+updateButton();
